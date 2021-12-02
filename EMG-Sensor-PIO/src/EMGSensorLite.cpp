4fba8c52eebf85df5449eab4e3e53f80bc5cf235
Janyboyfreak <janyboyfreak@gmail.com>
2021-12-02 17:25:13 +0100

    FlexiTimer and start byte added

--- EMG-Sensor-PIO/src/EMGSensorLite.cpp
+++ EMG-Sensor-PIO/src/EMGSensorLite.cpp
@@ -1,16 +1,13 @@
 #define LITE
 #ifdef LITE
 #include <Arduino.h>
+#include <FlexiTimer2.h>
 
-void setup(){
-    Serial.begin(2000000);
-    pinMode(A0, INPUT);
-    pinMode(A1, INPUT);
-    pinMode(A2, INPUT);
-    pinMode(A3, INPUT);
-}
+#define FREQ  1000.0
+#define SAMPLING_DELAY 1.0/FREQ // [ms]
 
-void loop(){
+void sample_data()
+{
     unsigned int ADC_Value = 0;
     if(false){
         Serial.print(analogRead(A0));
@@ -23,19 +20,35 @@ void loop(){
         Serial.print('\n');
     }
     else{
+        Serial.write(0xAA);
+        Serial.write(0xAA);
         for(unsigned char CurrentCh=0;CurrentCh<4;CurrentCh++){
         ADC_Value = analogRead(CurrentCh);
         Serial.write((unsigned char)((ADC_Value & 0xFF00) >> 8));
         Serial.write((unsigned char)(ADC_Value & 0x00FF));
         }
-        //Serial.write('\t');
-        //Serial.write(analogRead(A1));
-        //Serial.write('\t');
-        //Serial.write(analogRead(A2));
-        //Serial.write('\t');
-        //Serial.write(analogRead(A3));
-        Serial.write('\n');
     }
-    delay(2);
 }
+
+void setup(){
+    noInterrupts();
+
+    Serial.begin(2000000);
+
+    pinMode(A0, INPUT);
+    pinMode(A1, INPUT);
+    pinMode(A2, INPUT);
+    pinMode(A3, INPUT);
+
+    FlexiTimer2::set(1, SAMPLING_DELAY, sample_data);
+    FlexiTimer2::start();
+
+    interrupts();
+}
+
+void loop()
+{
+    __asm__ __volatile__ ("sleep");
+}
+
 #endif
